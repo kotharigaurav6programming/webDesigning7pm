5e13da3df7dbaad61690f4b7d02a877b9e0a279f
gauravKothari <kotharigaurav6@gmail.com>
2025-05-29 14:08:20 +0530

    javascript

---
+++ JavaScript/myScript.js
@@ -0,0 +1,3 @@
+document.write("This is an example of JavaScript<br>"); 
+document.write("<span style='color:red'>This is an example of JavaScript</span><br>");
+document.write("This is an example of JavaScript<br>");
