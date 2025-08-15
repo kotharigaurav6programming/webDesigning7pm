942c2fc202666afcf75a34363f5d8a4510a3127c
gauravKothari <kotharigaurav6@gmail.com>
2025-08-15 14:58:51 +0530

    js

--- JavaScript/17 Localstorage/myScript.js
+++ JavaScript/17 Localstorage/myScript.js
@@ -6,8 +6,39 @@ function addUser(){
     var address = document.getElementById("address").value;
 
     var userObj = {username,email,password,address};
-    userArray.push(userObj);
-    console.log("userArray : ",userArray);
-    localStorage.setItem("data",JSON.stringify(userArray));
-    
+    userArray = JSON.parse(localStorage.getItem("data"));
+
+    if(!userArray){
+        localStorage.setItem("data",JSON.stringify([...(userArray||[]),userObj]));
+        alert("Data Inserted Successfully");
+    }else{
+        var res = userArray.find((obj)=>{
+            return obj.email==userObj.email;
+        });
+        // console.log("object : ",res);
+        if(res)
+            alert("Email Id already Exist");
+        else{
+            localStorage.setItem("data",JSON.stringify([...(userArray||[]),userObj]));
+            alert("Data Inserted Successfully");
+        }
+    }
+}
+
+function checkLogin(){
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var userArray = JSON.parse(localStorage.getItem("data"));
+    var res = userArray.find((obj)=>{
+        return obj.email==email && obj.password==password;
+    });
+    if(res){
+        alert("Login Successfull");
+        window.location.href="file:///D:/desktop/CodingThinker/webDesigning7pm/JavaScript/17%20Localstorage/profile.html";
+    }
+    else{
+        alert("Error while Login");
+        window.location.reload();
+    }
+
 }
